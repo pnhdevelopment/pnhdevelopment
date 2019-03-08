daf6de67171fcc75707ecbb0f65272625128daee
Pascale <pnhdevelopment@gmail.com>
2019-03-08 17:17:40 +1100

    Added site screenshots, updated latest work secion on homepage

--- src/app/latest-work/latest-work.component.ts
+++ src/app/latest-work/latest-work.component.ts
@@ -15,45 +15,49 @@ export class LatestWorkComponent implements OnInit {
   works = [
   	{
   		"title": "Gorgeous Cupcakes",
-  		"screenshot": "http://via.placeholder.com/350x150",
+  		"screenshot": "https://via.placeholder.com/350x150",
   		"description": "A Wordpress ecommerce site dedicated to my favourite cupcake recipes.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"]
   	},
   	{
   		"title": "Scifi Film Festival",
-  		"screenshot": "http://via.placeholder.com/350x150",
+  		"screenshot": "https://via.placeholder.com/350x150",
   		"description": "A Wordpress site built specifically for the Sci Fi Film festival in Sydney.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"]
   	},
   	{
   		"title": "Bibletopia",
-  		"screenshot": "http://via.placeholder.com/350x150",
+  		"screenshot": "https://via.placeholder.com/350x150",
   		"description": "A personalised collection of educational resources dedicated to Christian scripture.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"]
   	},
   	{
   		"title": "Node Knowledgebase",
-  		"screenshot": "http://via.placeholder.com/350x150",
+  		"screenshot": "https://via.placeholder.com/350x150",
   		"description": "A collection of learning resources dedicated to the wonderful world of NodeJS.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"]
   	},
   	{
   		"title": "Guy Maston",
-  		"screenshot": "http://via.placeholder.com/350x150",
+  		"screenshot": "assets/guy-maston-screenshot.JPG",
   		"description": "An general ecommerce site. It sells baby products, electronics, books, fashion items and entertainment products.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"]
   	},
   	{
   		"title": "Youtube University",
-  		"screenshot": "http://via.placeholder.com/350x150",
+  		"screenshot": "assets/youtube-university-screenshot.jpg",
   		"description": "A collection of educational Youtube resources dedicated to my web design journey.",
-      "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"]
+      "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"],
+      "angularLink": "http://youtube-university-angular.pnhdevelopment.com", 
+      "reactLink": "http://youtube-university-react.pnhdevelopment.com"
   	},
   	{
   		"title": "My Recipes",
-  		"screenshot": "http://via.placeholder.com/350x150",
+  		"screenshot": "assets/my-recipes-screenshot.jpg",
   		"description": "A personalised collection of my favourite recipes.",
-      "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"]
+      "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"],
+      "angularLink": "http://my-recipes-angular.pnhdevelopment.com", 
+      "reactLink": "http://my-recipes-react.pnhdevelopment.com"
   	}
   ]
 

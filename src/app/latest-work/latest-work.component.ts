0a0f092502e83d94c0c49a9e283a478482da7169
Pascale <pnhdevelopment@gmail.com>
2019-03-30 23:50:19 +1100

    Made hero image and screenshots smaller

--- src/app/latest-work/latest-work.component.ts
+++ src/app/latest-work/latest-work.component.ts
@@ -19,21 +19,21 @@ export class LatestWorkComponent implements OnInit {
   works = [
   	{
   		"title": "Gorgeous Cupcakes",
-  		"screenshot": "assets/screenshots/gorgeous-cupcakes-snapshot.PNG",
+  		"screenshot": "assets/screenshots/gorgeous-cupcakes-snapshot.jpg",
   		"description": "A Wordpress ecommerce site dedicated to my favourite cupcake recipes.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"],
       "regularLink": "http://www.gorgeous-cupcakes.pnhdevelopment.com/"
   	},
   	{
   		"title": "Bibletopia",
-  		"screenshot": "assets/screenshots/bibletopia-screenshot.png",
+  		"screenshot": "assets/screenshots/bibletopia-screenshot.jpg",
   		"description": "A personalised collection of educational resources dedicated to Christian scripture.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"],
       "regularLink": "http://www.bibletopia.pnhdevelopment.com"
   	},
   	{
   		"title": "Node Knowledgebase",
-  		"screenshot": "assets/screenshots/node-knowledgebase.PNG",
+  		"screenshot": "assets/screenshots/node-knowledgebase.jpg",
   		"description": "A collection of learning resources dedicated to the wonderful world of NodeJS.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"],
       "angularLink": "https://www.node-knowledgebase-angular.pnhdevelopment.com", 
@@ -41,14 +41,14 @@ export class LatestWorkComponent implements OnInit {
   	},
   	{
   		"title": "Guy Maston",
-  		"screenshot": "assets/screenshots/guy-maston-screenshot.PNG",
+  		"screenshot": "assets/screenshots/guy-maston-screenshot.jpg",
   		"description": "An general ecommerce site. It sells baby products, electronics, books, fashion items and entertainment products.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"],
       "angularLink": "https://www.guy-maston-angular.pnhdevelopment.com",
   	},
   	{
   		"title": "Youtube University",
-  		"screenshot": "assets/screenshots/youtube-university-screenshot.PNG",
+  		"screenshot": "assets/screenshots/youtube-university-screenshot.jpg",
   		"description": "A collection of educational Youtube resources dedicated to my web design journey.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"],
       "angularLink": "https://www.youtube-university-angular.pnhdevelopment.com", 
@@ -56,7 +56,7 @@ export class LatestWorkComponent implements OnInit {
   	},
   	{
   		"title": "My Recipes",
-  		"screenshot": "assets/screenshots/my-recipes-screenshot.PNG",
+  		"screenshot": "assets/screenshots/my-recipes-screenshot.jpg",
   		"description": "A personalised collection of my favourite recipes.",
       "technologies": ["wordpress", "git", "bootstrap", "illustrator", "git"],
       "angularLink": "https://www.my-recipes-angular.pnhdevelopment.com", 
